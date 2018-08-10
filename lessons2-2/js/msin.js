e562f011f4fda008dd406ff920a187af17f7de73
Sibitceb <hemyli@bk.ru>
2018-08-10 21:23:53 +0700

    Доп урок 2

---
+++ lessons2-2/js/msin.js
@@ -0,0 +1,24 @@
+let week = ['Понидкльник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'];
+	i = 0;
+
+
+
+for (i = 0; i < 7; i++) {
+	if ([i] > 4) {
+		document.write('<b>' + week[i] + '</b>' + '<br>');
+	} else 	if (week[i] == 'Пятница') {
+			document.write('<i>' + week[i] + '</i>' + '<br>');
+		} else 
+			document.write(week[i] + '<br>');
+}
+
+console.log(week[i]);
+
+let arr = ['24144', '9252356', '38655', '4565352', '7352253', '4584235', '33264',];
+
+for (i = 0; i <7; i++) {
+	let text = arr[i];
+	if (text[0] == '3' || text[0] == '7') {
+		console.log(text);
+	}
+}
